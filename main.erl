c617bfb36b258ca2b895a73ed3c082d594e2ed47
Jacob Lorensen <jalor@yousee.dk>
2013-03-09 21:43:10 +0100

    Cleanup

--- main.erl
+++
@@ -1,9 +0,0 @@
--module(main).
--export([simulate/0]).
-
-simulate() ->
-    CmtsId = cmts,
-    LogFile = "/tmp/log.log",
-    cmts:start_link(CmtsId, LogFile),
-    CmId = cm1,
-    cm:start_link(CmtsId, CmId, {11,11,11,11,11,11}).
