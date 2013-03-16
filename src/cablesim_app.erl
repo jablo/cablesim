1471d19526ac857fb8d594d9c21a1aab25502e92
Jacob Lorensen <jalor@yousee.dk>
2013-03-16 15:30:28 +0100

    Cleanup

--- src/cablesim_app.erl
+++ src/cablesim_app.erl
@@ -4,7 +4,7 @@
 -include("device.hrl").
 
 %% Application callbacks
--export([start/2, stop/1, start_cablemodem/2, do_startcm/0]).
+-export([start/2, stop/1, start_cablemodem/2]).
 
 -export([mk_cms/4]).
 
@@ -12,87 +12,150 @@
 %% Application callbacks
 %% ===================================================================
 
+%% @doc
+%% Start the cablesim application. Starts a cablesim supervisor, and
+%% a some cable modem simulation processes.
+%% @spec start(_, _) -> {ok, Pid} | 
+%% @end
 start(_StartType, _StartArgs) ->
     error_logger:logfile({open, "/tmp/cablesim.log"}),
     X = cablesim_sup:start_link(),
-    simulate(1),
+    simulate(2),
     X.
 
 stop(_State) ->
     ok.
 
-%%
-%% Utitlity function to start a cable modem emulation
-%%
-start_cablemodem(Device, BehindDevs) ->
-    % first start the cable modem process
+%% @doc
+%% Create a cable modem transient server process
+%% @spec start_cm(Device, BehindDevs) -> {ok, Pid} | ignore | {error, Error}
+%% @end
+start_cm(Device, BehindDevs) ->
     supervisor:start_child(
       cablesim_sup, {Device#device.server_id,
                      {cm, start_link, [Device, BehindDevs]}, 
-                     transient, 5000, worker, dynamic}),
-    % then create cable modem's dhcp module
+                     transient, 5000, worker, dynamic}).
+
+%% @doc
+%% Create a dhcp client module server process for a Device. Returns ignore
+%% if this device does not have a dhcp module and thus no processes should
+%% be created. (This would be a very strange network device indeed).
+%% @spec start_dhcp(Device) -> { ok, Pid} | ignore | {error, Error}
+%% @end
+start_dhcp(Device) ->
     DeviceTempl = Device#device.template,
-    supervisor:start_child(
-      cablesim_sup, {Device#device.dhcp_client,
-                     {DeviceTempl#device_template.dhcp_module, start_link,
-                      [Device#device.dhcp_client, Device]},
-                     transient, 5000, worker, dynamic}),
-    % Create dhcp server component on all behind-devices
+    DhcpModule = DeviceTempl#device_template.dhcp_module,
+    case DhcpModule of
+        undefined -> ignore;
+        _ ->
+            Name = Device#device.dhcp_client,
+            supervisor:start_child(
+              cablesim_sup, {Name,
+                             {DhcpModule, start_link, [Name, Device]},
+                             transient, 5000, worker, dynamic})
+    end.
+
+%% @doc
+%% Create a tod client module server process for a Device. Returns ignore
+%% if this device does not have a tod module and thus no processes should
+%% be created.
+%% @spec start_tod(Device) -> { ok, Pid} | ignore | {error, Error}
+%% @end
+start_tod(Device) ->
+    DeviceTempl = Device#device.template,
+    ToDModule = DeviceTempl#device_template.tod_module,
+    case ToDModule of
+        undefined -> ignore;
+        _ ->
+            Name = Device#device.tod_client,
+            supervisor:start_child(
+              cablesim_sup, {Name,
+                             {ToDModule, start_link, [Name, Device]},
+                             transient, 5000, worker, dynamic}
+             )
+    end.
+
+%% @doc
+%% Create a tftp client module server process for a Device. Returns ignore
+%% if this device does not have a tftp module and thus no processes should
+%% be created.
+%% @spec start_tftp(Device) -> { ok, Pid} | ignore | {error, Error}
+%% @end
+start_tftp(Device) ->
+    DeviceTempl = Device#device.template,
+    TftpModule = DeviceTempl#device_template.tftp_module,
+    case TftpModule of
+        undefined -> ignore;
+        _ ->
+            Name = Device#device.tftp_client,
+            supervisor:start_child(
+              cablesim_sup, {Name,
+                             {TftpModule, start_link, [Name, Device]},
+                             transient, 5000, worker, dynamic}
+             )
+    end.
+
+%% @doc
+%% Create a full cable modem simulation, ie. create processes for
+%% the cable modem, it's dhcp client, tftp client, tod client
+%% and - recursively - create the same for any device running 
+%% behind the cable modem.
+%% @spec start_cablemodem(Device, BehindDevs) -> ok | ?
+%% @end
+start_cablemodem(Device, BehindDevs) ->
+    start_cm(Device, BehindDevs),
     lists:foreach(fun (F_D) ->
-                          F_DT = F_D#device.template,
-                          supervisor:start_child(
-                            cablesim_sup, {F_D#device.dhcp_client,
-                                           {F_DT#device_template.dhcp_module, 
-                                            start_link, [F_D#device.dhcp_client, F_D]},
-                                           transient, 5000, worker, dynamic})
+                          start_dhcp(F_D),
+                          start_tod(F_D),
+                          start_tftp(F_D)
                   end,
-                  BehindDevs).
-%%
-%% Utility function to start a cable modem emulation of 1 Netgear CG3000.
-%% 
-do_startcm() ->
-    [CMTempl, MTATempl, CPETempl] = device:cpedb(),
-    CM_Dev = #device{server_id = cm1, upstream_id = cmts, mac = {0,0,0,0,0,1}, 
-                     dhcp_client = cm1_dhcp,
-                     template = CMTempl},
-    MTA_Dev = #device{server_id = cm1_mta, upstream_id = cm1, mac = {0,0,0,0,0,2},
-                      dhcp_client = cm1_mtadhcp,
-                      template = MTATempl},
-    CPE_Dev = #device{server_id = cm1_cpe, upstream_id = cm1, mac = {0,0,0,0,0,3},
-                      dhcp_client = cm1_cpedhcp,
-                      template = CPETempl},
-    start_cablemodem(CM_Dev, [CPE_Dev,MTA_Dev]).
+                  [Device | BehindDevs]).
 
-simulate(N) ->
-    [CMTempl, MTATempl, CPETempl] = device:cpedb(),
-    mk_cms(N, CMTempl, MTATempl, CPETempl).
 
-mk_cms(0, _, _, _) -> ok;
-mk_cms(N, CMTempl, MTATempl, CPETempl) ->
-    CM_id = mk_atom(cm, N),
-    MTA_id = mk_atom(cm, N, mta),
-    CPE_id = mk_atom(cm, N, cpe),
-    CM_dhcp_id = mk_atom(cm, N, dhcp),
-    MTA_dhcp_id = mk_atom(cm, N, mtadhcp),
-    CPE_dhcp_id = mk_atom(cm, N, cpedhcp),
-    A = 3 * N div 200,
-    B = 3 * N rem 200,
-    io:format("Mac-part: ~p ~p ~p~n", [N, A, B]),
-    CM_Dev = #device{server_id = CM_id, upstream_id = cmts, mac = {0,0,0,0,A,B}, 
-                     dhcp_client = CM_dhcp_id,
-                     template = CMTempl},
-    MTA_Dev = #device{server_id = MTA_id, upstream_id = CM_id, mac = {0,0,0,0,A,B+1},
-                      dhcp_client = MTA_dhcp_id,
-                      template = MTATempl},
-    CPE_Dev = #device{server_id = CPE_id, upstream_id = CM_id, mac = {0,0,0,0,A,B+2},
-                      dhcp_client = CPE_dhcp_id,
-                      template = CPETempl},
-    start_cablemodem(CM_Dev, [CPE_Dev,MTA_Dev]),
-    cm:poweron(CM_id),
-    mk_cms(N-1, CMTempl, MTATempl, CPETempl).
-    
-    
-mk_atom(Prefix, N) ->
-   list_to_atom(atom_to_list(Prefix) ++ integer_to_list(N)).
+%% @doc
+%% Create a device record from a Process name, upstream
+%% process name, Mac address nad device template record.
+%% @spec mk_device(Name, Upstream, Mac, Templ) -> Device
+%% @end
+mk_device(Name, Upstream, Mac, Templ = #device_template{}) ->
+    #device{server_id = Name, upstream_id = Upstream, mac = Mac, 
+            dhcp_client = mk_atom(Name, dhcp),
+            tod_client = mk_atom(Name, tod),
+            tftp_client = mk_atom(Name, tftp),
+            template = Templ}.
+
+mk_atom(Prefix, N) when is_integer(N) ->
+   list_to_atom(atom_to_list(Prefix) ++ integer_to_list(N));
+mk_atom(Prefix, Postfix) when is_atom(Postfix) ->
+   list_to_atom(atom_to_list(Prefix) ++ atom_to_list(Postfix)).
 mk_atom(Prefix, N, Postfix) ->
    list_to_atom(atom_to_list(Prefix) ++ integer_to_list(N)  ++ "_" ++ atom_to_list(Postfix)).
+
+%% @doc
+%% Make a MAC address 
+%% @spec mk_mac({A,B,C,D}, N} -> {A,B,C,D,E,F}.
+%% @end
+mk_mac({A,B,C,D}, N) -> {A,B,C,D,N div 200, N rem 200}.
+
+%% @doc
+%% Create and start N cable modems with MTA and CPE behind it 
+%% based on specified CM, MTA and CPE device templates
+%% @spec _ -> ok.
+%% @end
+mk_cms(0, _, _, _) -> ok;
+mk_cms(N, CMTempl, MTATempl, CPETempl) ->
+    CM = mk_device(mk_atom(cm, N), cmts, mk_mac({0,0,0,0}, 3*N), CMTempl),
+    MTA = mk_device(mk_atom(cm, N, mta), CM#device.server_id, mk_mac({0,0,0,0}, 3*N+1), MTATempl),
+    CPE = mk_device(mk_atom(cm, N, cpe), CM#device.server_id, mk_mac({0,0,0,0}, 3*N+2), CPETempl),
+    start_cablemodem(CM, [MTA, CPE]),
+    cm:poweron(mk_atom(cm, N)),
+    mk_cms(N-1, CMTempl, MTATempl, CPETempl).
+
+%% @doc
+%% Start a simulation of a number of CG300 cable modems with
+%% built in MTA and CPE
+%% @spec N -> ok.
+%% @end
+simulate(N) ->
+    [CMT, MTAT, CPET] = device:cpedb(),
+    mk_cms(N, CMT, MTAT, CPET).
